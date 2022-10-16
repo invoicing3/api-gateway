c8072b3e63fe129cda523ef9c7565a8e94fb80f7
ali abdalla <ali.dev.sd@gmail.com>
2022-10-16 12:18:19 +0200

    add .env.example file

--- src/config/hosts.config.ts
+++ src/config/hosts.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs('hosts', () => ({
     iam: {
-        host: process.env.IAM_HOST,
-        port: process.env.PORT
+        host: process.env.IAM_MICROSERVICE_HOST,
+        port: process.env.IAM_MICROSERVICE_PORT
     }
 }));
